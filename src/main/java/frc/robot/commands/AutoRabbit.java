c1bc56f8238b5961f022eb9c746c33e7c151124e
ToddFerris <97573112+ToddFerris@users.noreply.github.com>
2023-12-12 18:30:05 -0800

    forgot to save

--- src/main/java/frc/robot/commands/AutoRabbit.java
+++ src/main/java/frc/robot/commands/AutoRabbit.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.RabbitDeploySubsystem;
@@ -14,21 +15,19 @@ import frc.robot.subsystems.SwerveSubsystem;
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
-public class AutoTest extends SequentialCommandGroup {
+public class AutoRabbit extends SequentialCommandGroup {
   /** Creates a new AutoTest. */
-  public AutoTest(SwerveSubsystem swerveSubsystem, RabbitDeploySubsystem rabbitDeploySubsystem, RabbitIntakeSubsystem rabbitIntakeSubsystem) {
+  public AutoRabbit(SwerveSubsystem swerveSubsystem, RabbitDeploySubsystem rabbitDeploySubsystem, RabbitIntakeSubsystem rabbitIntakeSubsystem) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
       new ResetOdometryCommand(swerveSubsystem),
       new DriveToDistanceCommand(swerveSubsystem, -Units.inchesToMeters(247), 0, 0, 5),
       new RabbitDeployCommand(rabbitDeploySubsystem, -18.1),
-
-      new WaitCommand(1),
-      
+      new InstantCommand(() -> rabbitIntakeSubsystem.startIntake(), rabbitIntakeSubsystem),
+      new WaitCommand(.5),
+      new InstantCommand(() -> rabbitIntakeSubsystem.startIntake(0), rabbitIntakeSubsystem),
       new RabbitDeployCommand(rabbitDeploySubsystem, 0),
-      new DriveToDistanceCommand(swerveSubsystem, 0, 0, 0, 5),
-
-    );
+      new DriveToDistanceCommand(swerveSubsystem, 0, 0, 0, 5));
   }
 }
